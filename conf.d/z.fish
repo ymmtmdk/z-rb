60e53ffa71af5894a6eb6fc95d4c6dce46c48ddf
ymmtmdk <ymmtmdk@gmail.com>
2024-02-04 17:44:56 +0900

    slim

--- conf.d/z.fish
+++ conf.d/z.fish
@@ -9,3 +9,19 @@ end
 function __z_on_variable_pwd --on-variable PWD
   __z_add
 end
+
+# autocdz
+function autocdz
+  if test (count $argv) -gt 1
+    echo "fish: Unknown command '$argv[1]'" >&2
+  else if test -d $argv
+    cd $argv
+  else if __z_move $argv
+    echo cd $PWD
+  end
+end
+
+function __fish_command_not_found_handler --on-event fish_command_not_found
+  autocdz $argv
+end
+
--- functions/__z_add.fish
+++ functions/__z_add.fish
@@ -1,17 +1,9 @@
 function __z_add -d "Add PATH to .z file"
   set -l path (dirname (status -f))
 
-  for i in $Z_EXCLUDE
-    if contains -- $PWD $i
-      return 0 #Path excluded
-    end
-  end
-
   set -l tmpfile (mktemp $Z_DATA.XXXXXX)
 
   if test -f $tmpfile
-    # awk -v path="$PWD" -v now=(date +%s) -F "|" -f $path/zadd.awk $Z_DATA ^ /dev/null > $tmpfile
-    # /usr/bin/ruby $path/zadd.rb --pwd="$PWD" --now=(date +%s) $Z_DATA ^ /dev/null > $tmpfile
     zadd --pwd="$PWD" --now=(date +%s) $Z_DATA ^ /dev/null > $tmpfile
     mv -f $tmpfile $Z_DATA
   end
